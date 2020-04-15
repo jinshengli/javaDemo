b8962794df956f96c5448ec804821f7593d3cd0e
jinshengli <1515026890@qq.com>
2020-04-15 11:01:55 +0800

    异常分类

---
+++ src/se/exception/KnowException.java
@@ -0,0 +1,20 @@
+package se.exception;
+
+/**
+ *  Throwable
+ *
+ *  异常分类： Error 和 Exception
+ *
+ *  Error 中的主要有 虚拟机异常：包括[StackOverFlow、OutOFMemoryException]
+ *
+ *  Exception:  分为checkedException:  是RuntimeException的这个大类
+ *
+ *  RunTimeException [NullPointException, IndexOutOfBoundsException ]。编译器无法预知的异常
+ *
+ *  uncheckedException是非RunTimeException的， IOException [EOFException、FileNotFoundException]
+ *  编译器预知的异常，这种异常必须在程序中处理，比如 throws, try...catch、提高代码的健壮性。
+ *
+ */
+
+public class KnowException {
+}
